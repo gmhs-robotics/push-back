17be8e69cbaeee44bd95b6157cdeba69314d54a8
elijah629 <62805599+elijah629@users.noreply.github.com>
2025-10-11 14:07:43 -0700

    do

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,26 @@
 #![no_main]
 #![no_std]
 
-mod forklift;
-mod odometry;
-
-use nalgebra::Vector3;
-use vexide::prelude::*;
-
-use crate::{
-    forklift::{Forklift, LiftLevel},
-    odometry::MecanumOdometry,
+use vexide::{
+    devices::{math::Point2, smart::GpsSensor},
+    prelude::*,
 };
+
+const MAX_WHEEL: f64 = Motor::V5_MAX_VOLTAGE * 0.8;
 
 pub struct Robot {
     controller: Controller,
 
-    lift: Motor,
+    gps: GpsSensor,
+
+    intake: Motor,
+    outtake: Motor,
+    router: Motor,
 
     left_front: Motor,
     left_back: Motor,
     right_front: Motor,
     right_back: Motor,
-
-    odometry: MecanumOdometry,
 }
 
 impl Compete for Robot {
@@ -39,72 +37,48 @@ impl Compete for Robot {
             let controller_state = self.controller.state().unwrap_or_default();
 
             // Forward/Back
-            let lx = controller_state.left_stick.y();
+            let lx = controller_state.right_stick.x();
 
             // Strafe
-            let ly = controller_state.left_stick.y();
+            let ly = controller_state.right_stick.y();
 
             // Rotation
-            let rx = controller_state.right_stick.x();
+            let rx = controller_state.left_stick.x();
 
-            let v_fl = ly - lx - rx;
-            let v_fr = ly + lx + rx;
-            let v_bl = ly + lx - rx;
-            let v_br = ly - lx + rx;
+            let v_fl = ly - lx + rx;
+            let v_fr = ly + lx - rx;
+            let v_bl = ly + lx + rx;
+            let v_br = ly - lx - rx;
 
-            self.left_front
-                .set_voltage(v_fl * Motor::V5_MAX_VOLTAGE)
-                .ok();
-            self.left_back
-                .set_voltage(v_bl * Motor::V5_MAX_VOLTAGE)
-                .ok();
-            self.right_front
-                .set_voltage(v_fr * Motor::V5_MAX_VOLTAGE)
-                .ok();
-            self.right_back
-                .set_voltage(v_br * Motor::V5_MAX_VOLTAGE)
-                .ok();
+            self.left_front.set_voltage(v_fl * MAX_WHEEL).ok();
+            self.left_back.set_voltage(v_bl * MAX_WHEEL).ok();
+            self.right_front.set_voltage(v_fr * MAX_WHEEL).ok();
+            self.right_back.set_voltage(v_br * MAX_WHEEL).ok();
 
-            let level = if controller_state.button_a.is_now_pressed() {
-                Some(LiftLevel::High)
-            } else if controller_state.button_b.is_now_pressed() {
-                Some(LiftLevel::Medium)
-            } else if controller_state.button_x.is_now_pressed() {
-                Some(LiftLevel::Low)
-            } else if controller_state.button_y.is_now_pressed() {
-                Some(LiftLevel::Floor)
+            let int_fw = controller_state.button_r1.is_pressed();
+            let int_bw = controller_state.button_l1.is_pressed();
+
+            let rou_fw = controller_state.button_r2.is_pressed();
+            let rou_bw = controller_state.button_l2.is_pressed();
+
+            if int_fw && !int_bw {
+                self.intake.set_voltage(Motor::V5_MAX_VOLTAGE).ok();
+                self.outtake.set_voltage(Motor::V5_MAX_VOLTAGE).ok();
+            } else if int_bw && !int_fw {
+                self.intake.set_voltage(-Motor::V5_MAX_VOLTAGE).ok();
+                self.outtake.set_voltage(-Motor::V5_MAX_VOLTAGE).ok();
             } else {
-                None
-            };
-
-            if let Some(lift_level) = level {
-                self.lift(lift_level).await.ok();
+                self.intake.set_voltage(0.).ok();
+                self.outtake.set_voltage(0.).ok();
             }
 
-            let lf = self
-                .left_front
-                .raw_position()
-                .map(|x| x.0)
-                .unwrap_or_default();
-            let rf = self
-                .right_front
-                .raw_position()
-                .map(|x| x.0)
-                .unwrap_or_default();
-            let lb = self
-                .left_back
-                .raw_position()
-                .map(|x| x.0)
-                .unwrap_or_default();
-            let rb = self
-                .right_back
-                .raw_position()
-                .map(|x| x.0)
-                .unwrap_or_default();
-
-            let pose = self.odometry.update(lf, rf, lb, rb);
-
-            println!("{}", pose);
+            if rou_fw && !rou_bw {
+                self.router.set_voltage(Motor::V5_MAX_VOLTAGE).ok();
+            } else if rou_bw && !rou_fw {
+                self.router.set_voltage(-Motor::V5_MAX_VOLTAGE).ok();
+            } else {
+                self.router.set_voltage(0.).ok();
+            }
 
             sleep(Controller::UPDATE_INTERVAL).await;
         }
@@ -117,28 +91,35 @@ async fn main(peripherals: Peripherals) {
 
     let controller = peripherals.primary_controller;
 
-    println!("[init.motor] init LF(12), LB(11), RF(2), RB(1), LIFT(5) on ports");
-    let left_front = Motor::new(peripherals.port_12, Gearset::Green, Direction::Forward);
-    let left_back = Motor::new(peripherals.port_11, Gearset::Green, Direction::Forward);
-    let right_front = Motor::new(peripherals.port_2, Gearset::Green, Direction::Reverse);
-    let right_back = Motor::new(peripherals.port_1, Gearset::Green, Direction::Reverse);
+    println!("[init.motor] init LF(10), LB(9), RF(1), RB(2), IN(11), OUT(3), ROU(4) on ports");
+    let left_front = Motor::new(peripherals.port_10, Gearset::Green, Direction::Forward);
+    let left_back = Motor::new(peripherals.port_9, Gearset::Green, Direction::Forward);
+    let right_front = Motor::new(peripherals.port_1, Gearset::Green, Direction::Reverse);
+    let right_back = Motor::new(peripherals.port_2, Gearset::Green, Direction::Reverse);
 
-    let lift = Motor::new(peripherals.port_5, Gearset::Green, Direction::Forward);
+    // Must move inverse to eachother
+    let intake = Motor::new(peripherals.port_11, Gearset::Green, Direction::Forward);
+    let outtake = Motor::new(peripherals.port_3, Gearset::Green, Direction::Reverse);
+
+    // Decides which hole the balls go out
+    let router = Motor::new(peripherals.port_4, Gearset::Green, Direction::Forward);
+
+    let gps = GpsSensor::new(
+        peripherals.port_15,
+        Point2 { x: 0., y: 0. },
+        Point2 { x: 0., y: 0. },
+        180.,
+    );
 
     let robot = Robot {
         controller,
 
-        odometry: MecanumOdometry::new(
-            [
-                left_front.raw_position().map(|x| x.0).unwrap_or_default(),
-                right_front.raw_position().map(|x| x.0).unwrap_or_default(),
-                left_back.raw_position().map(|x| x.0).unwrap_or_default(),
-                right_back.raw_position().map(|x| x.0).unwrap_or_default(),
-            ],
-            Vector3::zeros(),
-        ),
+        intake,
+        outtake,
+        router,
 
-        lift,
+        gps,
+
         left_front,
         left_back,
         right_front,
